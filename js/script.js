56cff8bf2f037a57f5719ef0578a96631b20706f
juliannabspence <julianna.spence@gmail.com>
2017-08-24 10:14:47 -0700

    feedback
    See BARRY call outs on HTML, CSS & JS
    
    Thanks!

---
+++ js/script.js
@@ -0,0 +1,18 @@
+$('document').ready(function(){
+
+// GALLERY PAGE JS ********
+
+// On page load, hide images in #photo-display
+	$("#photo-display").hide();	
+	// BARRY: I AM SHOCKED THAT I EVEN TYPED THIS IN RIGHT TO HIDE #PHOTO-DISPLAY BUT... WHAT DO I PUT IN THE BLANK SPACE NOW SO IT SNTO SO BLANK?
+
+
+// Display pins on pin map 
+// Detect when user clicks a city and display city images
+// On click of pin, load images for selected city
+// If user selects sydney pin, display .sydney
+	// $("#sydney").click()
+	// if (city == "sydney") { $("photo-display").show("class", "sydney")}
+
+
+});
